651af61dacaea366e80d4694250f878df923ab00
Nícolas Aigner <nicolas.aigner@gmail.com>
2021-07-12 11:27:10 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import {AlurakutMenu, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons';
+import {ProfileRelationsBoxWrapper} from '../src/components/ProfileRelations';
+
+function ProfileSidebar(propriedades) {
+    console.log(propriedades);
+    return (
+        <Box>
+            <img src={`https://github.com/${propriedades.githubUser}.png`} style={{borderRadius: '8px'}}/>
+        </Box>
+    )
+}
+
+export default function Home() {
+    const usuarioAleatorio = {
+        githubUser: 'nicolasaigner',
+        firstName: 'Nícolas',
+        lastName: 'Aigner'
+    };
+    const pessoasFavoritas = [
+        'juunegreiros',
+        'omariosouto',
+        'peas',
+        'rafaballerini',
+        'marcobrunodev',
+        'felipefialho'
+    ]
+
+    return (
+        <>
+            <AlurakutMenu/>
+            <MainGrid>
+                <div className="profileArea" style={{gridArea: 'profileArea'}}>
+                    <ProfileSidebar githubUser={usuarioAleatorio.githubUser}/>
+                </div>
+                <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+                    <Box>
+                        <h1 className="title">
+                            Bem vindo(a), {usuarioAleatorio.firstName}
+                        </h1>
+
+                        <OrkutNostalgicIconSet/>
+                    </Box>
+                </div>
+                <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+                    <ProfileRelationsBoxWrapper>
+                        <h2 className="smallTitle">
+                            Pessoas da comunidade ({pessoasFavoritas.length})
+                        </h2>
+
+                        <ul>
+                            {pessoasFavoritas.map((itemAtual) => {
+                                return (
+                                    <li>
+                                        <a href={`/users/${itemAtual}`} key={itemAtual}>
+                                            <img src={`https://github.com/${itemAtual}.png`}/>
+                                            <span>{itemAtual}</span>
+                                        </a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </ProfileRelationsBoxWrapper>
+                </div>
+            </MainGrid>
+        </>
+    )
+}
