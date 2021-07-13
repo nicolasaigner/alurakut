37340c5b984ebf74b4654e296c0ccb9ca7ef005d
Nícolas Aigner <nicolas.aigner@gmail.com>
2021-07-13 09:01:33 -0300

    Botões de menu no perfil do usuário

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import {AlurakutMenu, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons';
+import {AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons';
 import {ProfileRelationsBoxWrapper} from '../src/components/ProfileRelations';
 
 function ProfileSidebar(propriedades) {
@@ -8,16 +8,29 @@ function ProfileSidebar(propriedades) {
     return (
         <Box>
             <img src={`https://github.com/${propriedades.githubUser}.png`} style={{borderRadius: '8px'}}/>
+            <hr />
+
+            <p>
+                <a className="boxLink" href={`https://github.com/${propriedades.githubUser}`} target="_blank">
+                    @{propriedades.githubUser}
+                </a>
+            </p>
+
+            <hr />
+
+            <AlurakutProfileSidebarMenuDefault />
         </Box>
     )
 }
 
 export default function Home() {
+
     const usuarioAleatorio = {
         githubUser: 'nicolasaigner',
         firstName: 'Nícolas',
         lastName: 'Aigner'
     };
+
     const pessoasFavoritas = [
         'juunegreiros',
         'omariosouto',
