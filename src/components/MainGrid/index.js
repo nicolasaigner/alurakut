4e5b6fe39e40c2e55a4f21f9c730e4e839a0771e
Nícolas Aigner <nicolas.aigner@gmail.com>
2021-07-13 08:55:23 -0300

    Correção para o mainGrid ficar centralizado horizontalmente

--- src/components/MainGrid/index.js
+++ src/components/MainGrid/index.js
@@ -3,8 +3,7 @@ import styled from "styled-components";
 const MainGrid = styled.main`
   width: 100%;
   grid-gap: 10px;
-  margin-left: 0;
-  margin-right: 0;
+  margin: 0 auto;
   max-width: 500px;
   padding: 16px;
 
